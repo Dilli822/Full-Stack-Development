c14213c314124a9a81524ab715ad882a4a80db47
dilli <dillihangrae@gmail.com>
2023-05-26 08:54:55 +0545

    [added] social media app full stack development..

---
+++ backend/blogapp/models.py
@@ -0,0 +1,95 @@
+# # blogapp/models.py
+# from django.db import models
+# from django.contrib.auth.models import User
+# from django.utils.text import slugify
+
+# class Blog(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     authorName = models.CharField(max_length=100)
+#     image = models.ImageField(upload_to='blog_images', null=True, blank=True)
+#     likes = models.IntegerField(default=0)
+#     liked_by = models.ManyToManyField(User, related_name='liked_blogs', blank=True)
+#     liked_state = models.BooleanField(default=False)
+#     liked_by_current_user = models.ForeignKey(User, related_name='liked_by_current_user', on_delete=models.CASCADE, null=True, blank=True)
+#     slug = models.SlugField(unique=True, null=True, blank=True)
+#     url = models.URLField(unique=True, null=True, blank=True)
+
+#     def __str__(self):
+#         return str(self.title)
+
+#     def save(self, *args, **kwargs):
+#         if not self.slug:
+#             self.slug = slugify(self.title)
+#         if not self.url:
+#             self.url = f'http://localhost:8000/api/blog/{self.slug}'
+#         super().save(*args, **kwargs)
+        
+
+
+# # Comments section
+# class Comment(models.Model):
+#     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     comment_content = models.TextField()
+#     comment_created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"Comment by {self.author} on {self.blog}"
+    
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    authorName = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='blog_images', null=True, blank=True)
+    likes = models.IntegerField(default=0)
+    liked_by = models.ManyToManyField(User, related_name='liked_blogs', blank=True)
+    liked_state = models.BooleanField(default=False)
+    liked_by_current_user = models.ForeignKey(User, related_name='liked_by_current_user', on_delete=models.CASCADE, null=True, blank=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    url = models.URLField(unique=True, null=True, blank=True)
+    total_comments = models.IntegerField(default=0)
+    video = models.FileField(upload_to='blog_videos', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.title)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        if not self.url:
+            self.url = f'http://localhost:8000/api/blog/{self.slug}'
+        super().save(*args, **kwargs)
+
+    def update_total_comments(self):
+        self.total_comments = self.comments.count()
+        self.save()
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_content = models.TextField()
+    comment_created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.blog}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.blog.update_total_comments()
+
+    def delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+        self.blog.update_total_comments()
---
+++ backend/blogapp/serializers.py
@@ -0,0 +1,105 @@
+
+from rest_framework import serializers, status
+from django.contrib.auth.models import User
+from .models import Blog
+from rest_framework import serializers
+from django.urls import reverse
+from .models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Check if username already exists
+        if User.objects.filter(username=validated_data['username']).exists():
+            raise serializers.ValidationError({"username": "Username already taken"})
+        elif User.objects.filter(email=validated_data['email']).exists():
+            raise serializers.ValidationError({"email": "Email is already registered!"})
+        
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class CurrentUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'date_joined')
+
+class BlogSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
+
+    class Meta:
+        model = Blog
+        fields = ['id', 'author', 'author_id', 'title', 'content', 'author', 'authorName', 'created_at', 'updated_at', 'image', 'likes', 'liked_state', 'liked_by_current_user','liked_by','url', 'video']
+        read_only_fields = ['id','author']
+        extra_kwargs = {
+            'author': {'required': False},
+            'id': {'required': False},
+            'image': {'required': False},
+            'likes': {'required': False},
+            'liked_by': {'required': False},
+            'video': {'required': False},
+            
+        }
+        
+class CurrentUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'date_joined')
+
+
+class BlogLikesSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+    author_id = serializers.ReadOnlyField(source='author.id')
+    blog_name = serializers.ReadOnlyField(source='title')
+    likes = serializers.IntegerField(required=False)
+    liked_state = serializers.SerializerMethodField()
+    liked_by_current_user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Blog
+        fields = ('id','author_id', 'blog_name', 'likes', 'liked_state', 'liked_by_current_user', 'liked_by',  'updated_at', 'created_at', 'image', 'url', 'video')
+
+    def get_liked_state(self, obj):
+        user = self.context['request'].user
+        return user in obj.liked_by.all()
+
+    def get_liked_by_current_user(self, obj):
+        user = self.context['request'].user
+        return user.id in obj.liked_by.values_list('id', flat=True)
+
+
+class BlogLinkSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Blog
+        fields = ['id', 'title', 'author', 'created_at', 'updated_at',  'url']
+
+    def get_url(self, obj):
+        request = self.context.get('request')
+        if request is not None:
+            return reverse('blog-detail', kwargs={'pk': obj.pk})
+        return None
+
+class ShareUrlSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'blog', 'author', 'comment_content', 'comment_created_at')
